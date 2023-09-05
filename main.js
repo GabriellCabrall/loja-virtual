da299a355c278585a6390334de0b79807743e44d
gabriel cabral <gabrielbielcabral@gmail.com>
2023-09-04 21:21:21 -0300

    Estilizando o card do produto no carrinho

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ const catalogo = [
 
 for (const produtoCatalogo of catalogo) {
     const cartaoProduto = `<div class="border-solid border-2 border-sky-500 w-48 m-2" id="card-produto-${produtoCatalogo.id}">
-    <img src="./assets/img/${produtoCatalogo.imagem}" alt="Produto ${produtoCatalogo.id} do meu site" style="height: 300px">
+    <img src="./assets/img/${produtoCatalogo.imagem}" alt="Produto ${produtoCatalogo.nome}" style="height: 300px">
     <p class="marca">${produtoCatalogo.marca}</p>
     <p>${produtoCatalogo.nome}</p>
     <p>R$${produtoCatalogo.preco}</p>
